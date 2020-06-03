a0d14dc060b8969256c84020a5a13536a8e77361
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-06-03 15:36:04 +0200

    Session 05. Introspection and reflection

---
+++ session-05/task-01/coverage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#
+# The Introspective Style Description:
+#
+# - The problem is decomposed using some form of 
+#   abstraction (procedures, functions, objects, etc.)
+# 
+# - The abstractions have access to information about 
+#   themselves, although they cannot modify that information
+#
+#
+# The Reflective Style Description:
+# - The problem is decomposed using some form of 
+#   abstraction (procedures, functions, objects, etc.)
+# 
+# - The abstractions have access to information about 
+#   themselves, although they cannot modify that information
+# 
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv)
